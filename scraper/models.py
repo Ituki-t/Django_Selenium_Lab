6be5b49af8bed48b72a6daac6ff6922f6b35137d
Itsuki <sanndaittapandaotta@gmail.com>
2025-09-30 19:19:15 +0900

    スクレイピングしたシラバスの情報を格納するSYllabusモデルを作成

--- scraper/models.py
+++ scraper/models.py
@@ -10,3 +10,14 @@ class CeleryTestModel(models.Model):
         return self.text
     class Meta:
         db_table = "celery_test_model" # テーブル名を指定
+
+
+class Syllabus(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.course_name
+    class Meta:
+        db_table = "syllabus"
