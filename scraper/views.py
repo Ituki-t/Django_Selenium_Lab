1987a430c231f64c38c3c895e817f42257dd00d3
Itsuki <sanndaittapandaotta@gmail.com>
2025-10-01 10:46:57 +0900

    templateで講義一覧を取得することができた

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Syllabus
+
 
 # Create your views here.
+
+
+def course_list(request):
+    courses = Syllabus.objects.all()
+    
+    context = {
+        'courses': courses
+    }
+    
+    return render(request, 'scraper/course_list.html', context)
