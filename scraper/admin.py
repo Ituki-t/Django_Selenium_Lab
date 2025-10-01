234dd64e656e89da24a02c4ec0f6f3fbb0f2d9c7
Itsuki <sanndaittapandaotta@gmail.com>
2025-10-01 14:40:30 +0900

    Syllabusモデルをadminで管理できるように

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import CeleryTestModel
+from .models import CeleryTestModel, Syllabus
 
 # Register your models here.
 
 class CeleryTestModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'created_at')
 
+class SyllabusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course_name', 'course_url', 'course_year', 'created_at')
+
+
+
+
 admin.site.register(CeleryTestModel, CeleryTestModelAdmin)
+admin.site.register(Syllabus, SyllabusAdmin)
+
